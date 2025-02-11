fb0500eefdbe40edbaf584d4ea937575aab41a9c
Nicolò Pasini <nicolo.pasini@edreamsodigeo.com>
2025-02-11 23:21:22 +0100

    feat: displaying first page on launch

--- starGazers/SceneDelegate.swift
+++ starGazers/SceneDelegate.swift
@@ -10,6 +10,8 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
     
+    private static let repoSelectioStoryboardName = "RepositorySelection"
+    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let scene = (scene as? UIWindowScene) else { return }
         
@@ -18,7 +20,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     }
     
     func configureWindow() {
-        window?.rootViewController = UIViewController()
+        let viewModel = RepositorySelectorViewModel()
+        let repoSelectionStoryboard = UIStoryboard(name: Self.repoSelectioStoryboardName, bundle: .main)
+        window?.rootViewController = repoSelectionStoryboard.instantiateInitialViewController { RepositorySelectionViewController(coder: $0, viewModel: viewModel) }
         window?.makeKeyAndVisible()
     }
 }
