3f9fe9510cda72d81cfbd5959b732c9c22100f6b
Nicolò Pasini <nicolo.pasini@edreamsodigeo.com>
2025-02-11 22:45:54 +0100

    feat: migrate app life cycle from SwiftUI to UIKit

---
+++ starGazers/AppDelegate.swift
@@ -0,0 +1,11 @@
+//
+//  AppDelegate.swift
+//  starGazers
+//
+//  Created by nicolo.pasini on 11/02/25.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {}
---
+++ starGazers/SceneDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  SceneDelegate.swift
+//  starGazers
+//
+//  Created by nicolo.pasini on 11/02/25.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let scene = (scene as? UIWindowScene) else { return }
+        
+        window = UIWindow(windowScene: scene)
+        configureWindow()
+    }
+    
+    func configureWindow() {
+        window?.rootViewController = UIViewController()
+        window?.makeKeyAndVisible()
+    }
+}
